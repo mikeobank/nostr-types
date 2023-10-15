1471b37ebce3b5894c00795457cf42fa5944aa11
Mike O'Bank <mike.obank@pm.me>
2023-10-15 19:39:47 +0200

    Rollback 4fbf87b

--- src/lib/subtleCrypto.ts
+++ src/lib/subtleCrypto.ts
@@ -1,4 +1,4 @@
+import { webcrypto } from "crypto"
 import isBrowser from "./utils/isBrowser"
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export default isBrowser ? window.crypto.subtle : require("crypto").webcrypto.subtle
+export default isBrowser ? window.crypto.subtle : webcrypto.subtle
